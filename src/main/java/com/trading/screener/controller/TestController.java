c1b291bde3563f3570709c99d4b5a533fc1c241a
akdevops <akchougule5607@gmail.com>
2026-09-13 21:24:04 +0530

    Added Somechanges

--- src/main/java/com/trading/screener/controller/TestController.java
+++ src/main/java/com/trading/screener/controller/TestController.java
@@ -1,11 +1,13 @@
 package com.trading.screener.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("/test")
+@RequestMapping("/test")
+@CrossOrigin(origins = "*")
 public class TestController {
 
     @GetMapping("/hello")
